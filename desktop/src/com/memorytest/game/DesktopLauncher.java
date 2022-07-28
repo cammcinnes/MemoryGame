4526e78dec4b954470d9c1a2fcee23c5b9829296
cammcinnes <camtmcinnes@gmail.com>
2022-07-28 11:31:30 -0700

    Update DesktopLauncher.java
    I set up the window for the application.

--- desktop/src/com/memorytest/game/DesktopLauncher.java
+++ desktop/src/com/memorytest/game/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.memorytest.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.memorytest.game.MemoryTest;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(1600,800);
 		config.setForegroundFPS(60);
-		config.setTitle("memory-test-game");
+        config.useVsync(true);
+		config.setTitle("Memory Test");
 		new Lwjgl3Application(new MemoryTest(), config);
 	}
 }
